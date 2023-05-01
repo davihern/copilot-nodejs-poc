a06952be6660b8df80e4bde1edf59fe5e541b55b
David Hernandez <david.hernandez@microsoft.com>
2023-05-01 14:33:31 +0200

    Added a challenge net project

---
+++ challenges/net/ChallengeProject.Tests/ChallengeProjectTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ChallengeProject;
+using Xunit;
+
+namespace ChallengeProject.Tests
+{
+    public class ChallengeTests
+    {
+        private readonly Challenge _challenge;
+
+        public ChallengeTests()
+        {
+            _challenge = new Challenge();
+        }
+
+
+
+        [Fact]
+        public void ApplicationInsightsTest()
+        {
+            var result = _challenge.AddCustomEventUsingApplicationInsights();
+
+            Assert.True(result, $"Result should be true");
+        }
+    }
+}
---
+++ challenges/net/ChallengeProject/Challenge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ChallengeProject
+{
+    public class Challenge
+    {
+        public bool AddCustomEventUsingApplicationInsights()
+        {
+
+            return false;
+        }
+
+    }
+}
